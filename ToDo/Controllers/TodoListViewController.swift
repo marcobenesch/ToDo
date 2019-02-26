66a285da5df26f65f1132eca29d0145958685f36
Marco Benesch <marcobenesch@marcos-macbook-air.local>
2019-02-26 22:52:57 +0100

    added NSCoder to encode/decode for persistent local storage

--- ToDo/Controllers/TodoListViewController.swift
+++ ToDo/Controllers/TodoListViewController.swift
@@ -10,13 +10,17 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
+    // array that holds the items in the list
     var itemArray = [Item]()
-    let defaults = UserDefaults.standard
+    // path to the plist for persisted storage
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+
     
     // MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // 3 examples to start with
         let newItem = Item()
         newItem.title = "Kaufe Äpfel"
         itemArray.append(newItem)
@@ -28,34 +32,28 @@ class TodoListViewController: UITableViewController {
         let newItem3 = Item()
         newItem3.title = "Kaufe Getränke"
         itemArray.append(newItem3)
- 
-        if let items = UserDefaults.standard.array(forKey: "ToDoListArray") as? [Item] {
-            itemArray = items
-        }
-
+        
+        // get the saved items
+        loadItems()
         
     }
     
-    // MARK: - TableView Datasource Methods
+    // MARK: - TableView Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         let item = itemArray[indexPath.row]
-        
         cell.textLabel?.text = item.title
         cell.accessoryType = item.done ? .checkmark : .none
-        
         return cell
     }
     
-    // MARK: - TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
-        tableView.reloadData()
+        saveItems()
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
@@ -64,24 +62,42 @@ class TodoListViewController: UITableViewController {
         
         // var that will take the String from alertTextField inside the alert
         var textField = UITextField()
-        
         let alert = UIAlertController(title: "Neue Aufgabe:", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Hinzufügen", style: .default) { (action) in
-            
             let newItem = Item()
             newItem.title = textField.text!
             self.itemArray.append(newItem)
-            
-            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
-            self.tableView.reloadData()
+            self.saveItems()
         }
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Aufgabe"
             textField = alertTextField
         }
-        
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+    }
+    
+    // MARK: - Model manipulation functions
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error encoding itemArray, \(error)")
+        }
+        self.tableView.reloadData()
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding itemArray, \(error)")
+            }
+        }
     }
     
 }
