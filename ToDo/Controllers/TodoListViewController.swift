ef1e0565690bd5d0dae71593ada72b95dabd3c6f
Marco Benesch <marcobenesch@marcos-macbook-air.local>
2019-02-28 13:11:33 +0100

    changes to the navigation bar + included searchbar

--- ToDo/Controllers/TodoListViewController.swift
+++ ToDo/Controllers/TodoListViewController.swift
@@ -12,35 +12,103 @@ import CoreData
 class TodoListViewController: UITableViewController {
     
     // MARK: - Variables
+    
     // array that holds the items in the list
     var itemArray = [Item]()
+    // array that holds filtered items while search is active
+    var filteredItemsArray = [Item]()
     // the context of the database
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-
+    // search controller for the navigation bar
+    let searchController = UISearchController(searchResultsController: nil)
+    
     
     // MARK: - viewDidLoad
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // setting up navigation bar + integrated search
+        setupNavBar()
         
         // get the saved items from database
         loadItems()
         
+        
+        
+        
+    }
+    
+    // MARK: - NavBar and Search stuff
+    
+    func setupNavBar() {
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Suchen"
+        searchController.searchBar.tintColor = .white
+        
+        definesPresentationContext = true
+        
+        //test
+        let scb = searchController.searchBar
+        if let textfield = scb.value(forKey: "searchField") as? UITextField {
+            textfield.textColor = UIColor.blue
+            
+            if let backgroundview = textfield.subviews.first {
+                backgroundview.backgroundColor = UIColor.white
+                backgroundview.layer.cornerRadius = 10;
+                backgroundview.clipsToBounds = true;
+            }
+        }
+    }
+    
+    func searchBarIsEmpty() -> Bool {
+        // Returns true if the text is empty or nil
+        return searchController.searchBar.text?.isEmpty ?? true
+    }
+    
+    func filterContentForSearchText(_ searchText: String, scope: String = "All") {
+        filteredItemsArray = itemArray.filter({( item : Item) -> Bool in
+            return (item.title?.lowercased().contains(searchText.lowercased()))!
+        })
+        
+        tableView.reloadData()
+    }
+    
+    func isFiltering() -> Bool {
+        return searchController.isActive && !searchBarIsEmpty()
     }
     
     // MARK: - TableView Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // if search is currently filtering
+        if isFiltering() {
+            return filteredItemsArray.count
+        }
         return itemArray.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        let item = itemArray[indexPath.row]
+        let item: Item
+        
+        if isFiltering() {
+            item = filteredItemsArray[indexPath.row]
+        } else {
+            item = itemArray[indexPath.row]
+        }
+        
         cell.textLabel?.text = item.title
         cell.accessoryType = item.done ? .checkmark : .none
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        context.delete(itemArray[indexPath.row])
+//        itemArray.remove(at: indexPath.row)
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         saveItems()
         tableView.deselectRow(at: indexPath, animated: true)
@@ -77,14 +145,25 @@ class TodoListViewController: UITableViewController {
         self.tableView.reloadData()
     }
     
-    func loadItems() {
-        let request : NSFetchRequest<Item> = Item.fetchRequest()
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
         do {
             itemArray = try context.fetch(request)
         } catch {
             print("Error fetching data from context, \(error)")
         }
+        tableView.reloadData()
     }
     
 }
+
+// MARK: - Extensions
+
+// Updating the results for UISearchController
+extension TodoListViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchController.searchBar.text!)
+    }
+}
+
+
 
